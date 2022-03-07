040754ddc9b22b64bf14847d4c0f03ca138b59e3
Mariusz Wnek <wnek.mariusz@gmail.com>
2022-03-07 13:57:14 +0100

    Tokens added

--- webpack.config.js
+++ webpack.config.js
@@ -7,12 +7,27 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
   },
   module: {
-    rules: [
-      {
-        test: /\.(s(a|c)ss)$/,
-        use: ['style-loader','css-loader', 'sass-loader']
-      }
-    ]
+    rules: [{
+        test:/\.s[ac]ss$/i,
+        use: [
+          // Inserts all imported styles into the html document
+          { loader: "style-loader" },
+  
+          // Translates CSS into CommonJS
+          { loader: "css-loader" },
+  
+          // Compiles Sass to CSS
+          { loader: "sass-loader"},
+  
+          // Reads Sass vars from files or inlined in the options property
+          { loader: "@epegzz/sass-vars-loader", options: {
+            syntax: 'scss',
+            files: [
+              path.resolve(__dirname, 'src/tokens-transformed.json')
+            ]
+          }
+          }]
+      }]
     },
     resolve: {
         extensions: ['*', '.js']
